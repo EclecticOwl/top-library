a2abfc1667bb4deb5b53efd7443f350f7481dc86
Michael Tillman <mnamllit@gmail.com>
2022-03-21 16:18:03 -0500

    added a feature to display books

--- main.js
+++ main.js
@@ -14,3 +14,43 @@ class Book {
 book1 = new Book('The Hobbit', 'Tolkien', '300')
 book2 = new Book('Random Book', 'Random Author', '203')
 book3 = new Book('Another Book', 'Another Author', '250')
+
+myLibrary.push(book1)
+myLibrary.push(book2)
+myLibrary.push(book3)
+
+
+// Grabbing Library table and creating elements
+let libTable = document.querySelector('#library-table')
+
+let display = () => {
+    myLibrary.forEach( (book) => {
+        let libTr = document.createElement('tr')
+        let title = document.createElement('td')
+        let author = document.createElement('td')
+        let pages = document.createElement('td')
+
+        title.textContent += book.title
+        author.textContent += book.author
+        pages.textContent += book.pages
+
+        libTable.append(libTr)
+        libTr.append(title)
+        libTr.append(author)
+        libTr.append(pages)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
