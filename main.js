808ce083ab035dedd214677b69751731b861d79b
Michael Tillman <mnamllit@gmail.com>
2022-03-21 15:45:53 -0500

    added book class, array to hold books, and sample books

---
+++ main.js
@@ -0,0 +1,16 @@
+myLibrary = []
+
+
+// Book Class
+
+class Book {
+    constructor(title, author, pages) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+    }
+}
+
+book1 = new Book('The Hobbit', 'Tolkien', '300')
+book2 = new Book('Random Book', 'Random Author', '203')
+book3 = new Book('Another Book', 'Another Author', '250')
